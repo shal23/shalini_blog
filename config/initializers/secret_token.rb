0313542e08588dc8815cd6287d86ab2ce5c857c0
Shalini <shalini.dalal@yahoo.com.au>
2013-04-29 12:38:55 +1000

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShaliniBlog::Application.config.secret_token = 'fa3937e8085e50774c359c9f9812ce05054245d22cf0ce11ca1844a6f812d66d03234ea033a93ce6419b56fa91b212e638b6ca64563e4c6f43a3d2b539315cb2'
