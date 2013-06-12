57eae3908abec5b6193d089c3ce88e521c8259b0
shal23 <dr.shalinidalal@gmail.com>
2013-06-12 14:10:23 +1000

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ ShaliniBlog::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'gmail.com' }
+  config.action_mailer.default_url_options = { :host => 'shalininirwal.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
